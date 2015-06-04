1e5b872c380816c70259a14d0f533083055a64b4
Roman <roman.atachiants@gmail.com>
2015-06-04 12:35:07 +0100

    bucket rename

--- deploy.sh
+++ deploy.sh
@@ -2,14 +2,14 @@
 
 # Configuration
 # -------------
-# BUCKET: The S3 bucket to sync
+# APP_BUCKET: The S3 bucket to sync
 # AWS_ACCESS_KEY: The access key to use for S3
 # AWS_SECRET_KEY: The secret key to use for S3
 
 export AWSACCESSKEYID=$AWS_ACCESS_KEY
 export AWSSECRETACCESSKEY=$AWS_SECRET_KEY
 
-/usr/bin/s3fs -o allow_other -o use_cache=/tmp $BUCKET /data
+/usr/bin/s3fs -o allow_other -o use_cache=/tmp $APP_BUCKET /data
 
 if [[ $APPSTART ]]
   then
